f30af2b38e6f22fd251763d3d3dec7d79ed094ff
Luis Rodriguez <luisrod22@gmail.com>
2020-08-21 02:53:40 -0400

    Made this look better down to iPhone 5 screens.

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,22 @@
 var menuBtn = document.getElementsByClassName("menu-btn");
-var closeMenuBtn = document.getElementsByClassName("close-menu-btn");
-var mobileMenu = document.getElementsByClassName("mobile-menu");
+// var mobileLogoBtn = document.getElementsByClassName("mobile-logo");
+var mobileSideMenu = document.getElementById("mobile-side-menu");
 var mobileOpacityLayer = document.getElementsByClassName(
   "mobile-opacity-layer"
 );
 
 var clickedBtn = function () {
-  mobileMenu[0].classList.toggle("active");
+  mobileSideMenu.classList.toggle("active");
   mobileOpacityLayer[0].classList.toggle("active");
 };
 
+// var closeMobileSideMenu = function () {
+//   mobileSideMenu.classList.remove("active");
+//   mobileOpacityLayer[0].classList.remove("active");
+// };
+
 menuBtn[0].addEventListener("click", clickedBtn);
-closeMenuBtn[0].addEventListener("click", clickedBtn);
+// mobileLogoBtn[0].addEventListener("click", closeMobileSideMenu);
 document.querySelectorAll(".mobile-page-link").forEach((mobilePageLinkBtn) => {
   mobilePageLinkBtn.addEventListener("click", clickedBtn);
 });
